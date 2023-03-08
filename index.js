1eb349c2ca1f8a35d6aa89ccfc12f73c291ed8b6
[suryaprathapreddy07] <[spreddy8951@gmail.com]>
2023-03-08 13:41:03 +0530

    media coverage swiper is added

--- index.js
+++ index.js
@@ -1,39 +1,57 @@
-'use strict';
+"use strict";
+// import Swiper from Swiper/bundle
 
 // selecting elements
 
-let hamburger=document.querySelector('.bars')
-let navbar=document.querySelector('#navbar')
-let responsiveSidebar=document.querySelector('.responsive-navbar-sidebar')
-let partialSearchBar=document.querySelector('.partial-search-bar')
-let responsiveNav=document.querySelector('.responsive-navbar')
-let homeSection=document.querySelector('.home-section')
-let homeSectionInput=document.querySelector('.home-section-content-input')
+let hamburger = document.querySelector(".bars");
+let navbar = document.querySelector("#navbar");
+let responsiveSidebar = document.querySelector(".responsive-navbar-sidebar");
+let partialSearchBar = document.querySelector(".partial-search-bar");
+let responsiveNav = document.querySelector(".responsive-navbar");
+let homeSection = document.querySelector(".home-section");
+let homeSectionInput = document.querySelector(".home-section-content-input");
 
 // implementing hamburger  functionality
-hamburger.addEventListener('click',()=>{
-    responsiveNav.classList.toggle('hidden')
-    //setting to top for responsive navbar sidebar
-let navbarHeight=navbar.clientHeight
-responsiveSidebar.style.top=`${navbarHeight}px`
+hamburger.addEventListener("click", () => {
+  responsiveNav.classList.toggle("hidden");
+  hamburger.classList.toggle("open");
+  hamburger.style.color = "blue";
 
-
-})
-
-
-
-console.log(navbarHeight)
+  //setting to top for responsive navbar sidebar
+  let navbarHeight = navbar.clientHeight;
+  responsiveSidebar.style.top = `${navbarHeight}px`;
+});
 
 //implementing partial searchbar functionality on scroll
-window.addEventListener('scroll',()=>{
+window.addEventListener("scroll", () => {
+  if (Math.floor(homeSection.getBoundingClientRect().y) < -420) {
+    partialSearchBar.classList.remove("hidden");
+    // homeSectionInput.style.display='none'
+  } else {
+    partialSearchBar.classList.add("hidden");
+    // homeSectionInput.style.display='block'
+  }
+  console.log(homeSection.getBoundingClientRect());
+});
 
-    if(Math.floor(homeSection.getBoundingClientRect().y)<-420){
-        partialSearchBar.classList.remove('hidden')
-        // homeSectionInput.style.display='none'
-    }
-    else{
-        partialSearchBar.classList.add('hidden')
-        // homeSectionInput.style.display='block'
-    }
-    console.log(homeSection.getBoundingClientRect())
-})
+// implementing swiper functionality
+const swiper = new Swiper('.swiper', {
+    
+  
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.next',
+    prevEl: '.prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
